d13cb57a83924647c4c6d483a07c22510f6598af
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2021-10-15 09:59:16 +0200

    Update startupscript.sh

--- startupscript.sh
+++ startupscript.sh
@@ -12,6 +12,6 @@ sudo service nginx reload
 sudo sh -c "echo 'test-user soft nofile 30000' >> /etc/security/limits.conf" # for increasing soft file open limit for current user
 sudo sh -c "echo 'test-user hard nofile 30000' >> /etc/security/limits.conf" # for increasing hard file open limit for current user
 sysctl -p
-sudo sh -c "echo 'worker_rlimit_nofile 30000;' >> /etc/nginx/nginx.conf" # for allowing nginx worker to open more files
+sudo sh -c "echo 'worker_rlimit_nofile 40000;' >> /etc/nginx/nginx.conf" # for allowing nginx worker to open more files
 sudo sed -i 's/worker_connections 768;/worker_connections 20000;/g' /etc/nginx/nginx.conf # for allowing nginx workers to have more conncetions
 sudo service nginx restart
